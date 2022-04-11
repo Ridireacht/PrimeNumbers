c17952cf25ae4cc24b1a69e37dc86041e55c4eec
Ridireacht <ridiredire@gmail.com>
2022-04-11 21:30:24 +0300

    Added input and checks for calculation range

--- Program.cs
+++ Program.cs
@@ -6,7 +6,45 @@ namespace PrimeNumbers
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            string input;
+            int a, b;
+
+            // inputting and checking first var
+            while (true)
+            {
+                Console.Write("Input first value: ");
+                input = Console.ReadLine();
+
+                if (int.TryParse(input, out int x))
+                {
+                    a = x;
+                    break;
+                }
+
+                else
+                    Console.WriteLine("\nIncorrect input! Try again.");
+            }
+
+            Console.WriteLine();
+
+            // inputting and checking second var
+            while (true)
+            {
+                Console.Write("Input second value: ");
+                input = Console.ReadLine();
+
+                if (int.TryParse(input, out int x))
+                {
+                    b = x;
+                    break;
+                }
+
+                else
+                    Console.WriteLine("\nIncorrect input! Try again.");
+            }
+
+
         }
     }
 }
