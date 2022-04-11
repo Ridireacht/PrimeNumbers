693441ce61dea76ca2070f2aec521ec68a6759b7
Ridireacht <ridiredire@gmail.com>
2022-04-11 21:39:56 +0300

    Now positive input only; the order of vars will swap if incorrect

--- Program.cs
+++ Program.cs
@@ -10,13 +10,15 @@ namespace PrimeNumbers
             string input;
             int a, b;
 
+
+
             // inputting and checking first var
             while (true)
             {
                 Console.Write("Input first value: ");
                 input = Console.ReadLine();
 
-                if (int.TryParse(input, out int x))
+                if (int.TryParse(input, out int x) && (x >= 0))
                 {
                     a = x;
                     break;
@@ -34,7 +36,7 @@ namespace PrimeNumbers
                 Console.Write("Input second value: ");
                 input = Console.ReadLine();
 
-                if (int.TryParse(input, out int x))
+                if (int.TryParse(input, out int x) && (x >= 0))
                 {
                     b = x;
                     break;
@@ -43,6 +45,11 @@ namespace PrimeNumbers
                 else
                     Console.WriteLine("\nIncorrect input! Try again.");
             }
+
+            // swap order of vars if it's incorrect (using tuples)
+            if (a > b)
+                (a, b) = (b, a);
+
 
 
         }
