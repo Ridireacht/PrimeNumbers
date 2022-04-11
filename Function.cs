afaa1aef1f836a361b81b33edb7f2a3eccb80ca1
Ridireacht <ridiredire@gmail.com>
2022-04-11 21:52:50 +0300

    All the code has been moved to a separate class file

---
+++ Function.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimeNumbers
+{
+    class Function
+    {
+        private int[] primes;
+        private string input;
+        private int a, b;
+
+        public Function()
+        {
+            InputRange();
+        }
+
+        public void InputRange()
+        {
+            // inputting and checking first var
+            while (true)
+            {
+                Console.Write("Input first value: ");
+                input = Console.ReadLine();
+
+                if (int.TryParse(input, out int x) && (x >= 0))
+                {
+                    a = x;
+                    break;
+                }
+
+                else
+                    Console.WriteLine("\nIncorrect input! Try again.");
+            }
+
+            Console.WriteLine();
+
+            // inputting and checking second var
+            while (true)
+            {
+                Console.Write("Input second value: ");
+                input = Console.ReadLine();
+
+                if (int.TryParse(input, out int x) && (x >= 0))
+                {
+                    b = x;
+                    break;
+                }
+
+                else
+                    Console.WriteLine("\nIncorrect input! Try again.");
+            }
+
+            // swap order of vars if it's incorrect (using tuples)
+            if (a > b)
+                (a, b) = (b, a);
+        }
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -7,50 +7,7 @@ namespace PrimeNumbers
         static void Main(string[] args)
         {
 
-            string input;
-            int a, b;
-
-
-
-            // inputting and checking first var
-            while (true)
-            {
-                Console.Write("Input first value: ");
-                input = Console.ReadLine();
-
-                if (int.TryParse(input, out int x) && (x >= 0))
-                {
-                    a = x;
-                    break;
-                }
-
-                else
-                    Console.WriteLine("\nIncorrect input! Try again.");
-            }
-
-            Console.WriteLine();
-
-            // inputting and checking second var
-            while (true)
-            {
-                Console.Write("Input second value: ");
-                input = Console.ReadLine();
-
-                if (int.TryParse(input, out int x) && (x >= 0))
-                {
-                    b = x;
-                    break;
-                }
-
-                else
-                    Console.WriteLine("\nIncorrect input! Try again.");
-            }
-
-            // swap order of vars if it's incorrect (using tuples)
-            if (a > b)
-                (a, b) = (b, a);
-
-
+            Function f = new Function();
 
         }
     }
