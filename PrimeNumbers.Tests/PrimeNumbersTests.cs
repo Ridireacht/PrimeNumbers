52b63544bc04898c92308fa6e6f5fe0357ca82c7
Ridireacht <ridiredire@gmail.com>
2022-04-23 00:09:47 +0300

    Added tests for SetByInput()

--- PrimeNumbers.Tests/PrimeNumbersTests.cs
+++ PrimeNumbers.Tests/PrimeNumbersTests.cs
@@ -1,5 +1,7 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
 using System.Collections.Generic;
+using System.Text.RegularExpressions;
 
 
 namespace PrimeNumbers.Tests
@@ -8,10 +10,10 @@ namespace PrimeNumbers.Tests
     public class PrimeNumbersTests
     {
         [TestMethod]
-        public void isPrime_returnsTrue()
+        public void isPrime_randomPrimes_returnsTrue()
         {
             // arrange
-            List<int> nums = new List<int> { 32213, 85829, 91297, 50923, 92767, 70099, 51713, 84299, 74317, 52571 };
+            List<int> nums = new() { 32213, 85829, 91297, 50923, 92767, 70099, 51713, 84299, 74317, 52571 };
             bool flag = true;
 
 
@@ -25,15 +27,15 @@ namespace PrimeNumbers.Tests
 
 
             // assert
-            Assert.AreEqual(flag, true);
+            Assert.AreEqual(true, flag);
         }
 
 
         [TestMethod]
-        public void isPrime_returnsFalse()
+        public void isPrime__randomNonPrimes_returnsFalse()
         {
             // arrange
-            List<int> nums = new List<int> { 14133, 51781, 12072, 77405, 41456, 11989, 74389, 21387, 67406, 92025 };
+            List<int> nums = new() { 14133, 51781, 12072, 77405, 41456, 11989, 74389, 21387, 67406, 92025 };
             bool flag = true;
 
 
@@ -47,7 +49,56 @@ namespace PrimeNumbers.Tests
 
 
             // assert
-            Assert.AreEqual(flag, true);
+            Assert.AreEqual(true, flag);
+        }
+
+
+        [TestMethod]
+        public void SetByInput_noExceptionIsThrown()
+        {
+            // arrange
+            List<string> inputs = new() { "7", "12", "-5", "100000", "-121212", "sample_text", "8xybr8y3b1x8br8", "#!@#%*&$^)**!&?¹;%:+", "12 31 42", "yyy"};
+
+
+            // act & assert
+            try
+            {
+                foreach (string input in inputs)
+                    int.TryParse(input, out int x);
+
+                foreach (string input in inputs)
+                    Regex.IsMatch(input, "^y$");
+            }
+
+            catch (Exception ex)
+            {
+                Assert.Fail($"Expected no exceptions, but got {ex.Message}");
+            }
+
+        }
+
+
+        [TestMethod]
+        public void SetByInput_randomInput_correctChecks()
+        {
+            // arrange
+            List<string> inputs = new() { "7", "12", "y", "-5", "1.1", "4.6", "100000", "-121212", "y", "sample_text", "8xybr8y3b1x8br8", "nn", "#!@#%*&$^)**!&?¹;%:+", "n", "12 31 42", "yyy" };
+            int count_ints = 0;
+            int count_yes_not = 0;
+
+
+            // act
+            foreach (string input in inputs)
+                if (int.TryParse(input, out int x) && (x > 1))
+                    count_ints++;
+
+            foreach (string input in inputs)
+                if (Regex.IsMatch(input, "^y$") || Regex.IsMatch(input, "^n$"))
+                    count_yes_not++;
+
+
+            // assert
+            Assert.AreEqual(true, (count_ints == 3 && count_yes_not == 3));
         }
     }
 }
