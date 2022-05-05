362f68522de6ad8698ef2aa49e41d79ee8cc99e2
Ridireacht <ridiredire@gmail.com>
2022-05-05 23:58:31 +0300

    Removed unnecessary comments

--- GUI/PrimeNumbers/Form1.Designer.cs
+++ GUI/PrimeNumbers/Form1.Designer.cs
@@ -3,15 +3,8 @@ namespace PrimeNumbers
 {
     partial class Form1
     {
-        /// <summary>
-        /// Обязательная переменная конструктора.
-        /// </summary>
         private System.ComponentModel.IContainer components = null;
 
-        /// <summary>
-        /// Освободить все используемые ресурсы.
-        /// </summary>
-        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
         protected override void Dispose(bool disposing)
         {
             if (disposing && (components != null))
