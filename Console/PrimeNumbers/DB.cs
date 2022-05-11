5ff8def077acf98e6570f58a820b5600cbdc1ac5
Ridireacht <ridiredire@gmail.com>
2022-05-11 20:01:47 +0300

    Created separate class for DB operations

---
+++ Console/PrimeNumbers/DB.cs
@@ -0,0 +1,20 @@
+﻿using System.Diagnostics;
+using System.Text.RegularExpressions;
+using System.Runtime.CompilerServices;
+using Microsoft.Data.Sqlite;
+using PrimeNumbers.Properties;
+
+[assembly: InternalsVisibleTo("PrimeNumbers.Tests")]
+
+
+
+namespace PrimeNumbers
+{
+    internal class DB
+    {
+        private const string DB_NAME = "calculated_primes.db";
+
+
+
+    }
+}
