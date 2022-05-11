6f7dadba9b1e95148ea19de59589b456a0f87064
Ridireacht <ridiredire@gmail.com>
2022-05-11 19:39:10 +0300

    Created separate class for input/output

---
+++ Console/PrimeNumbers/IO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+[assembly: InternalsVisibleTo("PrimeNumbers.Tests")]
+
+
+namespace PrimeNumbers
+{
+    internal class IO
+    {
+
+    }
+}
