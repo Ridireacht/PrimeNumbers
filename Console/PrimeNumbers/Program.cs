7b0effa796d98d45e41db04aa2faebe4c3b8475b
Ridireacht <ridiredire@gmail.com>
2022-05-13 00:17:50 +0300

    Moved some code to functions for readability purposes

--- Console/PrimeNumbers/Program.cs
+++ Console/PrimeNumbers/Program.cs
@@ -21,29 +21,8 @@ List<int> primes = new();
 
 
 {
-    Console.WriteLine("Both of your range ends have to be >= 2.\n");
-
-    // getting range ends
-    IO.SetByInput(ref a, "Input first value: ");
-    IO.SetByInput(ref b, "Input second value: ");
-
-    // swap their if it's incorrect (using tuples)
-    if (a > b)
-        (a, b) = (b, a);
-
-    // getting conditions
-    IO.SetByInput(ref isOutput, "there be an output of calculated primes");
-    IO.SetByInput(ref isDatabase, "the program use DB");
-
-
-
-    if (isDatabase)
-    {
-        DB.CreateDatabase();
-        DB.GetFromDatabase(ref primes, a, b);
-    }
-
-    c.SetEnds(a, b);
+    Input();
+    Set();
 
 
 
@@ -86,4 +65,33 @@ List<int> primes = new();
     #if !DEBUG
         Console.ReadKey();
     #endif
+}
+
+
+void Input()
+{
+    Console.WriteLine("Both of your range ends have to be >= 2.\n");
+
+    // getting range ends
+    IO.SetByInput(ref a, "Input first value: ");
+    IO.SetByInput(ref b, "Input second value: ");
+
+    // swap their if it's incorrect (using tuples)
+    if (a > b)
+        (a, b) = (b, a);
+
+    // getting conditions
+    IO.SetByInput(ref isOutput, "there be an output of calculated primes");
+    IO.SetByInput(ref isDatabase, "the program use DB");
+}
+
+void Set()
+{
+    if (isDatabase)
+    {
+        DB.CreateDatabase();
+        DB.GetFromDatabase(ref primes, a, b);
+    }
+
+    c.SetEnds(a, b);
 }
