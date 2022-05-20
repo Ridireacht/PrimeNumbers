22b0b1a1e1173e2461fe9d4a88c28802c3fa4ced
Ridireacht <ridiredire@gmail.com>
2022-05-20 23:56:54 +0300

    Cleaned unnecessary comments

--- GUI/PrimeNumbers/Form1.Designer.cs
+++ GUI/PrimeNumbers/Form1.Designer.cs
@@ -2,15 +2,10 @@
 {
     partial class Form1
     {
-        /// <summary>
-        ///  Required designer variable.
-        /// </summary>
+
         private System.ComponentModel.IContainer components = null;
 
-        /// <summary>
-        ///  Clean up any resources being used.
-        /// </summary>
-        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+
         protected override void Dispose(bool disposing)
         {
             if (disposing && (components != null))
@@ -20,12 +15,7 @@
             base.Dispose(disposing);
         }
 
-        #region Windows Form Designer generated code
 
-        /// <summary>
-        ///  Required method for Designer support - do not modify
-        ///  the contents of this method with the code editor.
-        /// </summary>
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
@@ -34,6 +24,5 @@
             this.Text = "Form1";
         }
 
-        #endregion
     }
 }
