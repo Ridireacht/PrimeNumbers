67183bdda3ba7eb187bf8267a9bcd4c427f3ae39
Ridireacht <ridiredire@gmail.com>
2022-05-21 00:10:55 +0300

    Added some more elements

--- GUI/PrimeNumbers/Window.Designer.cs
+++ GUI/PrimeNumbers/Window.Designer.cs
@@ -21,6 +21,11 @@
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // textBox1
@@ -52,11 +57,63 @@
             this.label2.TabIndex = 2;
             this.label2.Text = "Program control menu";
             // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(405, 44);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(196, 25);
+            this.checkBox1.TabIndex = 3;
+            this.checkBox1.Text = "Show calculated primes";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(405, 75);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(233, 25);
+            this.checkBox2.TabIndex = 4;
+            this.checkBox2.Text = "Use database for calculations";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(405, 359);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(383, 30);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Calculate!";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(405, 395);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(189, 30);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Clear database";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(600, 395);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(188, 30);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "Save results to file";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
             // Window
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 21F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.textBox1);
@@ -70,5 +127,10 @@
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
     }
 }
