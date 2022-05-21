79900cd6656fe4ac3173ee8207ddf532231a080b
Ridireacht <ridiredire@gmail.com>
2022-05-21 18:56:48 +0300

    Checkbox processing

--- GUI/PrimeNumbers/Window.Designer.cs
+++ GUI/PrimeNumbers/Window.Designer.cs
@@ -93,6 +93,7 @@
             this.button1.TabIndex = 5;
             this.button1.Text = "Calculate!";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
--- GUI/PrimeNumbers/Window.cs
+++ GUI/PrimeNumbers/Window.cs
@@ -7,14 +7,49 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Diagnostics;
+
 
 namespace PrimeNumbers
 {
     public partial class Window : Form
     {
+
+        // global vars
+        int a = 0,
+            b = 0;
+
+        bool isOutput,
+             isDatabase,
+             isCorrect,
+             isToBeCleared;
+
+        // global objs
+        DB db = new();
+        Calculator c = new();
+
+        Stopwatch timer = new();
+        List<int> primes = new();
+
+
+
         public Window()
         {
             InitializeComponent();
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked)
+                isOutput = true;
+            else
+                isOutput = false;
+
+            if (checkBox2.Checked)
+                isDatabase = true;
+            else
+                isDatabase = false;
         }
 
     }
