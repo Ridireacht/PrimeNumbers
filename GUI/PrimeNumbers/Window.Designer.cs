76e14d9be21cfb258988da4c54705f166611c72f
Ridireacht <ridiredire@gmail.com>
2022-05-24 18:36:27 +0300

    Minor style

--- GUI/PrimeNumbers/Window.Designer.cs
+++ GUI/PrimeNumbers/Window.Designer.cs
@@ -219,7 +219,7 @@
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Window";
-            this.Text = "- Primes Calculator";
+            this.Text = "  Primes Calculator";
             this.ResumeLayout(false);
             this.PerformLayout();
 
