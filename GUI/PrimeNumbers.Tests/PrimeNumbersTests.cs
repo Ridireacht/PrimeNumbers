cf998a5bb4d795d3c8dd9cd623281d704efe4381
Ridireacht <ridiredire@gmail.com>
2022-05-24 18:47:22 +0300

    Template for unit-tests added

---
+++ GUI/PrimeNumbers.Tests/PrimeNumbersTests.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace PrimeNumbers.Tests
+{
+    [TestClass]
+    public class PrimeNumbersTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
