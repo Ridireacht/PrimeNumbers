4e4f1e179f94e4254f9b772d7aa9a985457aea65
Ridireacht <ridiredire@gmail.com>
2023-02-20 23:15:47 +0300

    Improved the UI

--- GUI/PrimeNumbers/Window.Designer.cs
+++ GUI/PrimeNumbers/Window.Designer.cs
@@ -41,40 +41,41 @@
             // textBox1
             // 
             this.textBox1.BackColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.textBox1.Location = new System.Drawing.Point(12, 33);
+            this.textBox1.Location = new System.Drawing.Point(12, 32);
             this.textBox1.Multiline = true;
             this.textBox1.Name = "textBox1";
             this.textBox1.ReadOnly = true;
             this.textBox1.RightToLeft = System.Windows.Forms.RightToLeft.No;
             this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
-            this.textBox1.Size = new System.Drawing.Size(387, 405);
+            this.textBox1.Size = new System.Drawing.Size(372, 389);
             this.textBox1.TabIndex = 0;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(136, 9);
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(131, 4);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(118, 21);
+            this.label1.Size = new System.Drawing.Size(136, 25);
             this.label1.TabIndex = 1;
             this.label1.Text = "Output window";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(534, 9);
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(508, 4);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(168, 21);
+            this.label2.Size = new System.Drawing.Size(192, 25);
             this.label2.TabIndex = 2;
             this.label2.Text = "Program control menu";
             // 
             // checkBox1
             // 
             this.checkBox1.AutoSize = true;
-            this.checkBox1.Location = new System.Drawing.Point(405, 50);
+            this.checkBox1.Location = new System.Drawing.Point(412, 47);
             this.checkBox1.Name = "checkBox1";
-            this.checkBox1.Size = new System.Drawing.Size(196, 25);
+            this.checkBox1.Size = new System.Drawing.Size(188, 24);
             this.checkBox1.TabIndex = 3;
             this.checkBox1.Text = "Show calculated primes";
             this.checkBox1.UseVisualStyleBackColor = true;
@@ -82,18 +83,18 @@
             // checkBox2
             // 
             this.checkBox2.AutoSize = true;
-            this.checkBox2.Location = new System.Drawing.Point(405, 81);
+            this.checkBox2.Location = new System.Drawing.Point(412, 77);
             this.checkBox2.Name = "checkBox2";
-            this.checkBox2.Size = new System.Drawing.Size(233, 25);
+            this.checkBox2.Size = new System.Drawing.Size(225, 24);
             this.checkBox2.TabIndex = 4;
             this.checkBox2.Text = "Use database for calculations";
             this.checkBox2.UseVisualStyleBackColor = true;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(405, 359);
+            this.button1.Location = new System.Drawing.Point(412, 344);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(383, 30);
+            this.button1.Size = new System.Drawing.Size(368, 29);
             this.button1.TabIndex = 5;
             this.button1.Text = "Calculate!";
             this.button1.UseVisualStyleBackColor = true;
@@ -101,9 +102,9 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(405, 395);
+            this.button2.Location = new System.Drawing.Point(412, 378);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(189, 30);
+            this.button2.Size = new System.Drawing.Size(181, 29);
             this.button2.TabIndex = 6;
             this.button2.Text = "Clear database";
             this.button2.UseVisualStyleBackColor = true;
@@ -111,9 +112,9 @@
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(600, 395);
+            this.button3.Location = new System.Drawing.Point(600, 379);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(188, 30);
+            this.button3.Size = new System.Drawing.Size(180, 29);
             this.button3.TabIndex = 7;
             this.button3.Text = "Save results to file";
             this.button3.UseVisualStyleBackColor = true;
@@ -122,9 +123,9 @@
             // radioButton1
             // 
             this.radioButton1.AutoSize = true;
-            this.radioButton1.Location = new System.Drawing.Point(405, 133);
+            this.radioButton1.Location = new System.Drawing.Point(412, 127);
             this.radioButton1.Name = "radioButton1";
-            this.radioButton1.Size = new System.Drawing.Size(175, 25);
+            this.radioButton1.Size = new System.Drawing.Size(167, 24);
             this.radioButton1.TabIndex = 8;
             this.radioButton1.TabStop = true;
             this.radioButton1.Text = "Use mono-threading";
@@ -133,9 +134,9 @@
             // radioButton2
             // 
             this.radioButton2.AutoSize = true;
-            this.radioButton2.Location = new System.Drawing.Point(405, 164);
+            this.radioButton2.Location = new System.Drawing.Point(412, 156);
             this.radioButton2.Name = "radioButton2";
-            this.radioButton2.Size = new System.Drawing.Size(170, 25);
+            this.radioButton2.Size = new System.Drawing.Size(162, 24);
             this.radioButton2.TabIndex = 9;
             this.radioButton2.TabStop = true;
             this.radioButton2.Text = "Use multi-threading";
@@ -144,9 +145,9 @@
             // radioButton3
             // 
             this.radioButton3.AutoSize = true;
-            this.radioButton3.Location = new System.Drawing.Point(405, 195);
+            this.radioButton3.Location = new System.Drawing.Point(412, 186);
             this.radioButton3.Name = "radioButton3";
-            this.radioButton3.Size = new System.Drawing.Size(192, 25);
+            this.radioButton3.Size = new System.Drawing.Size(186, 24);
             this.radioButton3.TabIndex = 10;
             this.radioButton3.TabStop = true;
             this.radioButton3.Text = "Let the program decide";
@@ -155,41 +156,41 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(405, 262);
+            this.label3.Location = new System.Drawing.Point(412, 251);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(114, 21);
+            this.label3.Size = new System.Drawing.Size(107, 20);
             this.label3.TabIndex = 11;
             this.label3.Text = "First range end";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(653, 262);
+            this.label4.Location = new System.Drawing.Point(650, 251);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(135, 21);
+            this.label4.Size = new System.Drawing.Size(129, 20);
             this.label4.TabIndex = 12;
             this.label4.Text = "Second range end";
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(405, 286);
+            this.textBox2.Location = new System.Drawing.Point(412, 274);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(130, 29);
+            this.textBox2.Size = new System.Drawing.Size(125, 27);
             this.textBox2.TabIndex = 13;
             // 
             // textBox3
             // 
-            this.textBox3.Location = new System.Drawing.Point(653, 286);
+            this.textBox3.Location = new System.Drawing.Point(650, 274);
             this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(130, 29);
+            this.textBox3.Size = new System.Drawing.Size(125, 27);
             this.textBox3.TabIndex = 14;
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(557, 289);
+            this.label5.Location = new System.Drawing.Point(558, 276);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(72, 21);
+            this.label5.Size = new System.Drawing.Size(69, 20);
             this.label5.TabIndex = 15;
             this.label5.Text = "up to the";
             // 
@@ -199,9 +200,9 @@
             // 
             // Window
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(125F, 125F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(120F, 120F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
-            this.ClientSize = new System.Drawing.Size(798, 448);
+            this.ClientSize = new System.Drawing.Size(791, 430);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.textBox3);
             this.Controls.Add(this.textBox2);
